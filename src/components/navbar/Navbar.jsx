eedb09f547cc0dd961fd95d6f43125f3e842fdc4
filippennsater <filip.pennsater@hotmail.com>
2024-06-17 20:08:49 +0200

    Fixed a new navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,67 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.scss';
 import { createContext } from 'react';
+import $ from 'jquery';
+
 
 export const ThemeContext = createContext(null);
 
-
 function Navbar() {
 
-    const [click, setClick] = useState(false);
-
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
-
-    const showButton = () => {
-        if (window.innerWidth <= 960) {
-        } else {
-        }
-    };
-
-
-    //so the button doesn' appear when you refresh page
     useEffect(() => {
-        showButton();
-    }, []);
+        // jQuery code
+        $('.navTrigger').click(function () {
+            $(this).toggleClass('active');
+            console.log("Clicked menu");
+            $("#mainListDiv").toggleClass("show_list");
+            $("#mainListDiv").fadeIn();
+        });
 
-    window.addEventListener('resize', showButton);
+        // Cleanup function to remove event handlers
+        return () => {
+            $('.navTrigger').off('click');
+        };
+    }, []);
 
     return (
 
         <>
-            <nav className="navbar">
-                <div className='navbar-container'>
-                    <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-                        Portfolio <i className="fas fa-book"></i>
-                    </Link>
-                    <div className='menu-icon' onClick={handleClick}>
-                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+            <nav class="nav">
+                <div class="container">
+                    <div class="logo">
+                        <Link to='/' >
+                            SP Media and health
+                        </Link>
                     </div>
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-
-                        <li className='nav-item'>
-                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                    <div id="mainListDiv" class="main_list">
+                        <ul class="navlinks">
+                            <li><Link to='/' >
                                 Home
-                            </Link>
-                        </li>
-
-                       {/**  <li className='nav-item'>
-                            <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+                            </Link></li>
+                            <li><Link to='/about' >
                                 About
-                            </Link>
-                        </li>
-
-                        <li className='nav-item'>
-                            <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
+                            </Link></li>
+                            <li><Link to='/services' >
+                                Services
+                            </Link></li>
+                            <li><Link to='/contact' >
                                 Contact
-                            </Link>
-                        </li>*/}
-
-                    </ul>
-
+                            </Link></li>
+                        </ul>
+                    </div>
+                    <span class="navTrigger">
+                        <i></i>
+                        <i></i>
+                        <i></i>
+                    </span>
                 </div>
-
             </nav>
         </>
     );
