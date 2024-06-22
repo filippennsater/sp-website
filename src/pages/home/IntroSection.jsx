801a86e707d27259b2a88618ddad3df10c85fe62
filippennsater <filip.pennsater@hotmail.com>
2024-06-22 22:59:02 +0200

    added an animation for a picture in IntroSection

--- src/pages/home/IntroSection.jsx
+++ src/pages/home/IntroSection.jsx
@@ -1,39 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../../App.css';
 import './IntroSection.scss';
-
-
-
+import DisplacementEffect from './DisplacementEffect';
 
 function IntroSection() {
+  const [trigger, setTrigger] = useState(false);
+  const sectionRef = useRef(null);
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setTrigger(true);
+          observer.disconnect();
+        }
+      },
+      {
+        threshold: 0.5,
+      }
+    );
 
-    return (
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
 
-    <div className='intr-container'>
+    return () => {
+      if (observer && observer.current) {
+        observer.disconnect();
+      }
+    };
+  }, []);
 
-            <div className='intrc-container'>
-
-
-                    <div className='intr-bottom-container'>
-
-                        <div className='intr-text-container'>
-                            <div className='intrctext'>Välkommen till min hemsida! Jag heter Susanne Pennsäter och är legitimerad KBT-terapeut. Här kan du få hjälp med att hantera stress, ångest, depression och andra livsutmaningar genom kognitiv beteendeterapi (KBT). Jag erbjuder individuella samtal anpassade efter dina unika behov och mål. Utforska sidan för mer information om mina tjänster och hur vi tillsammans kan arbeta för ditt välbefinnande.</div>
-                        </div>
-
-                        <div className='intr-circle-container'>
-                            <div className='intr-outer-circle'>
-                                <div className='intr-circle'/>
-                            </div>
-                        </div>
-                        
-                    </div>
-
+  return (
+    <div className='intr-container' ref={sectionRef}>
+      <DisplacementEffect trigger={trigger} />
+      <div className='intrc-container'>
+        <div className='intr-bottom-container'>
+          <div className='intr-text-container'>
+            <div className='intrctext'>
+              Välkommen till min hemsida! Jag heter Susanne Pennsäter och är legitimerad KBT-terapeut. Här kan du få hjälp med att hantera stress, ångest, depression och andra livsutmaningar genom kognitiv beteendeterapi (KBT). Jag erbjuder individuella samtal anpassade efter dina unika behov och mål. Utforska sidan för mer information om mina tjänster och hur vi tillsammans kan arbeta för ditt välbefinnande.
             </div>
-            
+          </div>
+          <div className='intr-circle-container'>
+            <div className='intr-outer-circle'>
+              <div className='intr-circle' style={{ mask: 'url(#circleMask)', WebkitMask: 'url(#circleMask)' }} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-
-    )
+  );
 }
 
-export default IntroSection
+export default IntroSection;
