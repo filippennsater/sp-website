c9e522fb72c523f7d559e9e01d316f16430a87a0
filippennsater <filip.pennsater@hotmail.com>
2024-06-26 21:06:22 +0200

    fixed the contactinfo with a new map

--- src/pages/contact/ContactSection.jsx
+++ src/pages/contact/ContactSection.jsx
@@ -2,18 +2,9 @@ import React, { useState } from 'react';
 import '../../App.css';
 import './ContactSection.scss';
 
-import MapComponent from './MapComponent';
 
 function ContactSection() {
 
-    const [showRedText, setShowRedText] = useState(false);
-
-    const handleSettingsClick = () => {
-        setShowRedText(true);
-        setTimeout(() => {
-            setShowRedText(false);
-        }, 3000);
-    };
 
     return (
 
@@ -21,16 +12,16 @@ function ContactSection() {
 
             <div className='contact-info-wrap'>
 
-                
-                <div className='right-container'>
+
+                <div className='left-container'>
 
                     <div className='h30'>
-                    <br />Phone
+                        <br />Telefon
                     </div>
 
                     <div className='p30'>
                         000-000 00 00<br />
-                        Our phone hours are between 9:00 AM and 5:00 PM, Monday through Friday.
+                        Våra telefontider är mellan 9:00 och 17:00, måndag till fredag.
                     </div>
 
 
@@ -40,7 +31,7 @@ function ContactSection() {
                     </div>
 
                     <div className='p30'>
-                        PHILIP & PEN<br />
+                        SP-Health<br />
                         Make-Believe Lane 00<br />
                         000 00 LUND
                     </div>
@@ -48,49 +39,58 @@ function ContactSection() {
 
 
                     <div className='h30'>
-                        Socials
+                        Kontakta oss!
                     </div>
 
                     <div className='p30'>
-                        Follow us on our <a href="https://www.instagram.com/filippdev/" target="blank">social media!</a>
-                    </div>
-
-
-
-                    <div className='h30'>
-                        Poetry competition 2024!
-                    </div>
-
-                    <div className='p30'>
-                        Here, you can submit your entries for our poetry competition.<br />By participating, you agree that we reserve the right to publish your texts on our website.<br /><br />
+                        Nedan kan du snabbt skriva ett meddelande till oss!<br /><br />
                     </div>
 
                     <form>
-                        <label>
-                            Name<br />
-                            <input className='input-competition' type="text" name="name" /><br />
-                        </label>
-                        <label>
-                            Email<br />
-                            <input className='input-competition' type="text" name="name" /><br />
-                        </label>
-                        <label>
-                            Phonenumber<br />
-                            <input className='input-competition' type="text" name="name" /><br /><br />
-                        </label>
-                        
-                        <input type="file" />
-                        <input type="submit" value="Submit" onClick={handleSettingsClick}/>
-                    </form>
+                        <div className='small-header-wrap-suggestion'>
+                            Namn
+                        </div>
+                        <div className='suggestion-input-wrap'>
+                            <input
+                                className='input-suggestion'
+                                type="text"
+                                name="text"
+                            />
+                        </div>
+                        <div className='small-header-wrap-suggestion'>
+                            Mail
+                        </div>
+                        <div className='suggestion-input-wrap'>
+                            <input
+                                className='input-suggestion'
+                                type="text"
+                                name="text"
+                            />
+                        </div>
 
-                    <div className='redTextContact-container'>
-                        {showRedText && <div className="redTextContact">This is only a demo</div>}
-                    </div>
+                        <div className='small-header-wrap-suggestion'>
+                            Meddelande
+                        </div>
+                        <div className='suggestion-input-wrap'>
+                            <textarea
+                                className='input-suggestion2'
+                                type="message"
+                                name="message"
+                            />
+                        </div>
+
+
+                        <input className='suggestion-button cursor-pointer' type="submit" value="Skicka" />
+                    </form>
 
                 </div>
 
-                <MapComponent />
 
+                <div className='right-container'>
+                    <div className="map-container" dangerouslySetInnerHTML={{
+                        __html: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1123.9993771180305!2d13.197386428504018!3d55.70639341291944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465397cf7560906d%3A0x9de6e629c239cde3!2sBarnahus%20Lund!5e0!3m2!1ssv!2sse!4v1719426937163!5m2!1ssv!2sse" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+                    }} />
+                </div>
             </div>
 
         </div>
