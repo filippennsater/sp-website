74a7670fb773e0d5dd3fc62f67772a94641796e9
filippennsater <filip.pennsater@hotmail.com>
2024-06-27 22:34:05 +0200

    just changed som info

--- src/pages/home/IntroSection.jsx
+++ src/pages/home/IntroSection.jsx
@@ -38,7 +38,7 @@ function IntroSection() {
         <div className='intr-bottom-container'>
           <div className='intr-text-container'>
             <div className='intrctext'>
-              Välkommen till min hemsida! Jag heter Susanne Pennsäter och är legitimerad KBT-terapeut. Här kan du få hjälp med att hantera stress, ångest, depression och andra livsutmaningar genom kognitiv beteendeterapi (KBT). Jag erbjuder individuella samtal anpassade efter dina unika behov och mål. Utforska sidan för mer information om mina tjänster och hur vi tillsammans kan arbeta för ditt välbefinnande.
+              Välkommen till min hemsida! Jag heter Susanne Pennsäter och är KBT-terapeut. Här kan du få hjälp med att hantera stress, ångest, depression och andra livsutmaningar genom kognitiv beteendeterapi (KBT). Jag erbjuder individuella samtal anpassade efter dina unika behov och mål. Utforska sidan för mer information om mina tjänster och hur vi tillsammans kan arbeta för ditt välbefinnande.
             </div>
           </div>
           <div className='intr-circle-container'>
