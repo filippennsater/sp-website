b64a2f4e4c9df8a2b0a130fac0345979572cd9b6
filippennsater <filip.pennsater@hotmail.com>
2024-07-05 02:51:59 +0200

    changed the styling for every service and added the last serviceimages

--- src/components/serviceComponents/ServicesListItem.jsx
+++ src/components/serviceComponents/ServicesListItem.jsx
@@ -17,23 +17,14 @@ function ServicesListItem(props) {
                     className="serv-image"
                 />
 
-                <div className="serv-left-container">
+                <div className="serv-right-container">
                     <div className="serv-header">
                         {props.title}
-                    </div>
-                    {/*<div className="serv-header2">
-                        {props.guest}
-                    </div>*/}
-                </div>
-
-                <div className="serv-right-container">
-                    <div className="serv-text">
-                        {props.description}
                     </div>
                         <Link className='serv-button-container' to={`/service/${props.id}`}>
                             <div className="serv-button">Mer information&nbsp;&nbsp;<i className="fa-solid fa-play" /></div>
                         </Link>
-                </div>
+                        </div>
 
             </div>
         </>
