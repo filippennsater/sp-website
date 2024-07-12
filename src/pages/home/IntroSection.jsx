936573672c8bebe646b4532fa31c43f549a9bd2d
filippennsater <filip.pennsater@hotmail.com>
2024-07-12 23:23:39 +0200

    fixed so the image in IntroSection adjusts on top of the text when in mobile use

--- src/pages/home/IntroSection.jsx
+++ src/pages/home/IntroSection.jsx
@@ -35,16 +35,17 @@ function IntroSection() {
     <div className='intr-container' ref={sectionRef}>
       <DisplacementEffect trigger={trigger} />
       <div className='intrc-container'>
-        <div className='intr-bottom-container'>
-          <div className='intr-text-container'>
-            <div className='intrctext'>
-              Välkommen till min hemsida! Jag heter Susanne Pennsäter och är KBT-terapeut. Här kan du få hjälp med att hantera stress, ångest, depression och andra livsutmaningar genom kognitiv beteendeterapi (KBT). Jag erbjuder individuella samtal anpassade efter dina unika behov och mål. Utforska sidan för mer information om mina tjänster och hur vi tillsammans kan arbeta för ditt välbefinnande.
-            </div>
-          </div>
-          <div className='intr-circle-container'>
-              <div className='intr-circle' style={{ mask: 'url(#circleMask)', WebkitMask: 'url(#circleMask)' }} />
+
+        <div className='intr-circle-container'>
+          <div className='intr-circle' style={{ mask: 'url(#circleMask)', WebkitMask: 'url(#circleMask)' }} />
+        </div>
+        
+        <div className='intr-text-container'>
+          <div className='intrctext'>
+            Välkommen till min hemsida! Jag heter Susanne Pennsäter och är KBT-terapeut. Här kan du få hjälp med att hantera stress, ångest, depression och andra livsutmaningar genom kognitiv beteendeterapi (KBT). Jag erbjuder individuella samtal anpassade efter dina unika behov och mål. Utforska sidan för mer information om mina tjänster och hur vi tillsammans kan arbeta för ditt välbefinnande.
           </div>
         </div>
+
       </div>
     </div>
   );
