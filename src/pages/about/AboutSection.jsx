dcf3057df3d48d50bd60a6657ca66687d1627fc6
filippennsater <filip.pennsater@hotmail.com>
2024-07-19 23:50:18 +0200

    styling some cards for AboutSection

--- src/pages/about/AboutSection.jsx
+++ src/pages/about/AboutSection.jsx
@@ -6,10 +6,23 @@ function AboutSection() {
     return (
 
         <div className='about-container'>
-            
-            <div className='bl-text-container'>
 
-                <div className='abouttext'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada. Nullam vehicula, nisl sit amet vestibulum gravida, ligula sapien ultricies mi, non egestas dolor justo vel purus. Suspendisse potenti. Fusce aliquam nunc ac dolor fermentum, at facilisis purus venenatis. Curabitur id lorem nec nulla facilisis volutpat. Sed consequat metus nec arcu fermentum, et tincidunt ex ultricies. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ut dolor vel arcu hendrerit fringilla a sed tortor. Vestibulum et mauris vitae nulla tempus dignissim.</div>
+            <div className='cards-container'>
+
+                <div className='abouttext'>
+                    <div className='info-card1'>
+
+                    </div>
+                    <div className='info-card2'>
+
+                    </div>
+                    <div className='info-card3'>
+
+                    </div>
+                    <div className='info-card4'>
+
+                    </div>
+                </div>
 
             </div>
 
