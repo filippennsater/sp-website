0b9545afe2457fd5497a0e84effd5277c6eaf643
filippennsater <filip.pennsater@hotmail.com>
2024-07-25 23:49:43 +0200

    added some information in the AboutSection

--- src/pages/about/AboutSection.jsx
+++ src/pages/about/AboutSection.jsx
@@ -9,19 +9,32 @@ function AboutSection() {
 
             <div className='cards-container'>
 
-                
-                    <div className='info-card1'>
 
-                    </div>
-                    <div className='info-card2'>
+                <div className='info-card1'>
 
-                    </div>
-                    <div className='info-card3'>
+                    <ul>
+                        <div className='about-header'>OM</div>
+                        <div className='about-text'>Jag arbetar med evidensbaserad kognitiv beteendeterapi (KBT) och familjeterapi (IBCT)</div>
+                    </ul>
+                </div>
+                <div className='info-card2'>
 
-                    </div>
-                    <div className='info-card4'>
-
-                    </div>
+                </div>
+                <div className='info-card3'>
+                    <ul>
+                        <div className='about-text'>Med över 20 års erfarenhet från behandling, samtal.</div>
+                        <div className='about-text'>Över 20 års erfarenhet av arbetsplatsrelaterad problematik från företagshälsovård. Som utredning och bedömning av konflikter, svåra samtal, utredning och bedömning av kränkningsanmälan, arbetsförmågebedömningar.</div>
+                    </ul>
+                </div>
+                <div className='info-card4'>
+                    <ul>
+                        <div className='about-header'>CV</div>
+                        <li className='about-text'>KBT-steg 1 Psykologpartners</li>
+                        <li className='about-text'>IBCT Psykologpartners</li>
+                        <li className='about-text'>ICF- Karolinska institutet, arbetsförmågebedömning</li>
+                        <li className='about-text'>Medicinskt ledningsansvar, Sveriges företagshälsovårds förening</li>
+                    </ul>
+                </div>
 
             </div>
 
