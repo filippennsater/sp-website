d001ce8d6b21df86e6cad6cfa6c7e63e7b486ed4
filippennsater <filip.pennsater@hotmail.com>
2024-07-26 01:13:51 +0200

    fixed the text with padding and sizes in AboutSection

--- src/pages/about/AboutSection.jsx
+++ src/pages/about/AboutSection.jsx
@@ -9,26 +9,24 @@ function AboutSection() {
 
             <div className='cards-container'>
 
-
                 <div className='info-card1'>
-
-                    <ul>
+                <div className='header-container'>
                         <div className='about-header'>OM</div>
-                        <div className='about-text'>Jag arbetar med evidensbaserad kognitiv beteendeterapi (KBT) och familjeterapi (IBCT)</div>
-                    </ul>
+                    </div>
+                    <div className='about-text'>Jag arbetar med evidensbaserad kognitiv beteendeterapi (KBT) och familjeterapi (IBCT)</div>
                 </div>
                 <div className='info-card2'>
 
                 </div>
                 <div className='info-card3'>
-                    <ul>
-                        <div className='about-text'>Med över 20 års erfarenhet från behandling, samtal.</div>
-                        <div className='about-text'>Över 20 års erfarenhet av arbetsplatsrelaterad problematik från företagshälsovård. Som utredning och bedömning av konflikter, svåra samtal, utredning och bedömning av kränkningsanmälan, arbetsförmågebedömningar.</div>
-                    </ul>
+                    <div className='about-text'>Med över 20 års erfarenhet från behandling, samtal.</div>
+                    <div className='about-text'>Över 20 års erfarenhet av arbetsplatsrelaterad problematik från företagshälsovård. Som utredning och bedömning av konflikter, svåra samtal, utredning och bedömning av kränkningsanmälan, arbetsförmågebedömningar.</div>
                 </div>
                 <div className='info-card4'>
-                    <ul>
+                    <div className='header-container'>
                         <div className='about-header'>CV</div>
+                    </div>
+                    <ul>
                         <li className='about-text'>KBT-steg 1 Psykologpartners</li>
                         <li className='about-text'>IBCT Psykologpartners</li>
                         <li className='about-text'>ICF- Karolinska institutet, arbetsförmågebedömning</li>
