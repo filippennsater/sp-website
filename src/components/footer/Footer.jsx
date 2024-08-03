fdd8a5257424948d98a6cab3737da7ef62d7888f
filippennsater <filip.pennsater@hotmail.com>
2024-08-04 00:31:03 +0200

    fixed the footer so things are centered, also changed the border-style in AboutSection

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -24,10 +24,9 @@ function Footer() {
                     </div>
 
                     <div className='footer-link-items'>
-                        <h2>Rättigheter</h2>
-                        <Link to='https://opensource-7d53e.web.app' target='_blank'>Open source</Link>
-                        <Link to='https://mediainformation-ca3b9.web.app' target='_blank'>AI created media</Link>
-                        <Link to='https://userinformation-76154.web.app' target='_blank'>ToS and PP</Link>
+                        <h2>Kontakt</h2>
+                        <div className='p3'>0702884886</div>
+                        <div className='p3'>-@gmail.com</div>
                     </div>
 
                 </div>
@@ -35,16 +34,17 @@ function Footer() {
                 <div className='footer-link-wrapper'>
 
                     <div className='footer-link-items'>
+                        <h2>Rättigheter</h2>
+                        <Link to='https://opensource-7d53e.web.app' target='_blank'>Open source</Link>
+                        <Link to='https://mediainformation-ca3b9.web.app' target='_blank'>AI created media</Link>
+                        <Link to='https://userinformation-76154.web.app' target='_blank'>ToS and PP</Link>
+                    </div>
+
+                    <div className='footer-link-items'>
                         <h2>Länkar</h2>
                         <Link to='https://www.linkedin.com/in/susanne-penns%C3%A4ter-2a0636138/' target='_blank'>LinkedIn</Link>
                         <Link to='' target='_blank'>Google business</Link>
 
-                    </div>
-
-                    <div className='footer-link-items'>
-                        <h2>Kontakt</h2>
-                        <div className='p3'>0702884886</div>
-                        <div className='p3'>-@gmail.com</div>
                     </div>
 
                 </div>
@@ -57,7 +57,7 @@ function Footer() {
                 <div className='social-media-wrap'>
 
                     <Link to='/' className='footer-logo' />
-                    
+
                     <small className='website-rights'>© 2024</small>
 
                     <div className='social-icons'>
